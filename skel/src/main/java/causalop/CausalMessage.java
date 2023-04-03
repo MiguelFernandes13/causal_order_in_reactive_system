4f50e52f894a7dadf166e743398b8d26e5d3589a
Miguel Ângelo Ferreira Fernandes <62012969+MiguelFernandes13@users.noreply.github.com>
2023-04-03 17:04:29 +0100

    Add files via upload

---
+++ skel/src/main/java/causalop/CausalMessage.java
@@ -0,0 +1,29 @@
+package causalop;
+
+public class CausalMessage<T> implements Comparable<CausalMessage<T>> {
+    int j, v[];
+    public T payload;
+
+    public CausalMessage(T payload, int j, int... v) {
+        this.payload = payload;
+        this.j = j;
+        this.v = v;
+    }
+    
+    @Override
+    public int compareTo(CausalMessage<T> arg0) {
+        if (arg0 instanceof CausalMessage) {
+            CausalMessage<T> m = (CausalMessage<T>) arg0;
+            if (m.j == j) {
+                for (int i = 0; i < v.length; i++) {
+                    if (m.v[i] != v[i])
+                        return 0;
+                }
+                return 1;
+            }
+        }
+        return 0;        
+    }
+
+    
+}
---
+++ skel/src/main/java/causalop/CausalOperator.java
@@ -0,0 +1,91 @@
+package causalop;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.ObservableOperator;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.observers.DisposableObserver;
+
+public class CausalOperator<T> implements ObservableOperator<T, CausalMessage<T>> {
+    private final int n;
+    private int[] v;
+    private Set<CausalMessage<T>> q;
+
+    public CausalOperator(int n) {
+        this.n = n;
+        this.v = new int[n];
+        for (int i = 0; i < n; i++) {
+            v[i] = 0;
+        }
+        this.q = new TreeSet<>();
+    }
+
+    public boolean isCausal(CausalMessage<T> m) {
+        boolean causal = true;
+        if (v[m.j] + 1 == m.v[m.j]) {
+            for(int i = 0; i < n; i++) {
+                if (i != m.j && v[i] < m.v[i]) {
+                    causal = false;
+                    break;
+                }
+            }
+        }
+        else
+            causal = false;
+        return causal;
+    }
+
+    public boolean isDuplicated(CausalMessage<T> m) {
+        boolean duplicated = false;
+        if (v[m.j] + 1 > m.v[m.j]) {
+            duplicated = true;
+        }
+        return duplicated;
+    }
+
+    @Override
+    public @NonNull Observer<? super CausalMessage<T>> apply(@NonNull Observer<? super T> down) throws Throwable {
+        return new DisposableObserver<CausalMessage<T>>() {
+            @Override
+            public void onNext(@NonNull CausalMessage<T> m) {
+                if (isCausal(m)){
+                    v[m.j]++;
+                    down.onNext(m.payload);
+                    boolean repeat = true;
+                    while (repeat){
+                        repeat = false;
+                        for (CausalMessage<T> m2 : q) {
+                            if (isCausal(m2)){
+                                v[m2.j]++;
+                                q.remove(m2);
+                                down.onNext(m2.payload);
+                                repeat = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if (!isDuplicated(m))
+                    q.add(m);
+
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                down.onError(e); // FIXME
+            }
+
+            @Override
+            public void onComplete() {
+                down.onComplete(); // FIXME
+            }
+        };
+    }
+}
